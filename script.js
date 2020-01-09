658ef23b32eda616af6cedf53298c6c3b85b607c
Danny Cairns <dpcairns@gmail.com>
2020-01-08 20:57:20 -0800

    add cooking analogy

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Ingredients
+
 // go get the box where we want to put the new name
 const nameBox = document.getElementById('the-name');
 
@@ -7,10 +9,12 @@ const myInput = document.getElementById('bobby');
 // go get the badly named button for the submit
 const button = document.getElementById('bobby-button');
 
+// instructions
 const changeValue = () => {
     // when the user clicks, set the name box's text content to whatever the current value of the input is
     nameBox.textContent = myInput.value
 }
 
+// cooking
 // add an event listener to the button
 button.addEventListener('click', changeValue)
